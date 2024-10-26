e0e50145a75eb8265ef12cd44356d372ed745de0
marytishphillips <mphillip@trinity.edu>
2024-10-25 23:38:51 -0500

    Contractor Profile

--- db-project/src/containers/ContProfile.jsx
+++ db-project/src/containers/ContProfile.jsx
@@ -2,7 +2,20 @@ import React from 'react'
  
 const ContProfile = () => {
   return (
+    <>
         <h3>My Profile</h3>
+        <h4>First Name</h4>
+        <p>Mary</p>
+        <h4>Last Name</h4>
+        <p>Phillips</p>
+        <h4>Username</h4>
+        <p>mphillip</p>
+        <h4>Rate ($/hr)</h4>
+        <p>30.00</p>
+        <h4>Resume</h4>
+        <p>...</p>
+        <br></br><br></br>
+    </>
   );
 }
  
