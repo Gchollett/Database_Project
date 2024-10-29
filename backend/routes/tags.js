3491c4975bfb36d37469593092934aa307367a80
garrett <garrettchollett@yahoo.com>
2024-10-28 20:24:58 -0500

    Added Tags

---
+++ backend/routes/tags.js
@@ -0,0 +1,31 @@
+var express = require('express');
+const { PrismaClient } = require('@prisma/client');
+// const 
+var router = express.Router();
+
+const prisma = new PrismaClient();
+
+/**
+ * @openapi
+ * /tags:
+ *  get:
+ *      tags:
+ *          - Tags
+ *      summary: "Gets all tags"
+ *      responses:
+ *          200:
+ *              description: "Succressfuly retrieved all tags"
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              properties:
+ *                                  name:
+ *                                      type: string
+ *                                      default: "Software"
+ */
+router.get('/',async (req,res) => {
+    res.status(200).send(await prisma.tag.findMany())
+})
